66107826c58e8fb018f4f2268569fb5886c47cb8
Chenoa Siegenthaler <csiegenthaler@hedgeye.com>
2011-11-30 11:25:47 -0500

    Ruby 1.8 compatibility

--- spec/lib/chart_spec.rb
+++ spec/lib/chart_spec.rb
@@ -9,15 +9,15 @@ describe Chart do
 
   describe '#url' do
     it "should return a url with all the default parameters if given no others" do
-      url = Chart.new(symbol: 'abc').url
+      url = Chart.new(:symbol => 'abc').url
 
       some_defaults = {
-        webmasterId: "500",
-        chscale: '5d',
-        chtype: 'AreaChart',
-        chfrm: 'ffffff',
-        chbdron: 'on',
-        chhig: '250',
+        :webmasterId => "500",
+        :chscale => '5d',
+        :chtype => 'AreaChart',
+        :chfrm => 'ffffff',
+        :chbdron => 'on',
+        :chhig => '250',
       }
 
       some_defaults.each do |k,v|
@@ -26,14 +26,14 @@ describe Chart do
     end
 
     it "should give a url containing all the given parameters" do
-      custom_params = { symbol: 'abc',
-                        webmasterId: '321',
-                        chscale: '2d',
-                        chpcon: 'off',
-                        chtype: 'candle',
-                        chmrg: '7',
-                        chhig: '100',
-                        chwid: '150'
+      custom_params = { :symbol => 'abc',
+                        :webmasterId => '321',
+                        :chscale => '2d',
+                        :chpcon => 'off',
+                        :chtype => 'candle',
+                        :chmrg => '7',
+                        :chhig => '100',
+                        :chwid => '150'
       }
 
       url = Chart.new(custom_params).url
